cb05a114c46f71e0d69b7460e2a58736aa6b5868
Curasao <rudarya4@gmail.com>
2025-11-03 02:40:02 +0300

    Add conftest and tests.py

--- ui/pages/test_2gis.py
+++ ui/pages/test_2gis.py
@@ -1,14 +1,13 @@
-from selene import browser, be, have, command
+from selene import browser, be, have
 from allure import step
 
 class FilterPage:
     def open(self):
-        browser.open('/ruza/search/')
-        browser.execute_script('window.scrollBy(0, 500)')
+        browser.open('/ruza/')
         return self
 
-    def fill_search(self,query):
-        browser.element('input[placeholder="Поиск в 2ГИС"]').should(be.blank).type(query).press_enter()
+    def fill_search(self, query):
+        browser.element('input[placeholder*="Поиск"]').should(be.visible).set_value(query).press_enter()
         return self
 
     def dostavka_button(self):
@@ -22,4 +21,30 @@ class FilterPage:
     def price_button(self, min_price=180, max_price=500):
         browser.element('input._tupxg39').should(be.visible).clear().type(str(min_price))
         browser.element('input._1kvvnrkq').should(be.visible).clear().type(str(max_price)).press_enter()
+        return self
+
+class Realty_Page:
+
+    def realty_open(self):
+        browser.open('/ruza/realty/sale')
+        return self
+
+
+    def realty_price_button(self, min_price=180, max_price=500):
+        browser.element('input._tupxg39').should(be.visible).clear().type(str(min_price)).press_enter()
+        browser.element('input._1kvvnrkq').should(be.visible).clear().type(str(max_price)).press_enter()
+        return self
+
+
+
+    def realty_should_be_loaded(self):
+        browser.element('h1, h2').should(have.text('Продажа недвижимости'))
+        browser.element('button, [role="button"], a').with_(timeout=10).should(have.text('Фильтры'))
+        return self
+
+    def realty_search(self, query, min_price=1800000, max_price=5000000):
+        browser.open('https://2gis.ru/ruza/realty/sale')
+        browser.element('input[placeholder*="Район, улица, дом..."]').should(be.visible).set_value(query).press_enter()
+        browser.element('input._1qyj3m0').should(be.visible).clear().type(str(min_price)).press_enter()
+        browser.element('input._1qyj3m0').should(be.visible).clear().type(str(max_price)).press_enter()
         return self
--- ui/tests/tests_2gis.py
+++ ui/tests/tests_2gis.py
@@ -1,12 +1,30 @@
+from ui.pages.test_2gis import Realty_Page
 from ui.pages.test_2gis import FilterPage
 
-
-def test_form_submission(setup_browser):
-    registration_page = FilterPage()
+def test_form_submission(setup_browser_filter):
+    filter_page = FilterPage()
 
     # Открываем страницу
-    registration_page.open()
-    registration_page.fill_search("Кофейни")
-    registration_page.dostavka_button()
-    registration_page.rate_button()
+    filter_page.open()
+    filter_page.fill_search("Кофейни")
+    filter_page.dostavka_button()
+    filter_page.rate_button()
+    filter_page.price_button()
+
+
+def test_realty_search(setup_browser_realty):
+    realty_page = Realty_Page()
+    realty_page.realty_search("Тучково")
+
+
+def test_realty_filters_price_and_rooms(setup_browser_realty):
+    realty_page = Realty_Page()
+    realty_page.realty_open()
+    realty_page.realty_price_button(400, 800)  # Пример установки цены
+
+
+
+
+
+    
 
